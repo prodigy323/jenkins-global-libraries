d413c4e1b166064145eaed6198157211b3895d10
Joe Hong <jhng323@gmail.com>
2019-03-30 10:08:16 -0400

    initial setup for glPublishToNexus

--- vars/glPublishToNexus.groovy
+++ vars/glPublishToNexus.groovy
@@ -5,6 +5,16 @@ def call(config[:]) {
     def settingsXml = config.settingsXml
     def skipTest = config.skipTest ?: false
 
-    sh "mvn -B -DskipTests=true clean install"
+    def pom = readMavenPom file: 'pom.xml'
+
+    withCredentials([string(credentialsId: "${githubToken}", variable: 'TOKEN')]) {
+        return sh(script: """
+            mvn -B -DskipTests=${skipTest} -s ${settingsXml} deploy
+            git config user.email "jhng323@gmail.com"
+            git config user.name "prodigy323"
+            git tag -a ${pom.version} -m 'create tag ${pom.version}'
+            git push --tags https://\${TOKEN}@github.com/prodigy323/basic-springboot-project.git
+        """, returnStdout: true)
+    }
 
 }
