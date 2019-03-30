0a1e83ec09e9c42e03b848d32250dc586f4c7519
Joe Hong <jhng323@gmail.com>
2019-03-30 10:11:11 -0400

    remove git commands

--- vars/glPublishToNexus.groovy
+++ vars/glPublishToNexus.groovy
@@ -7,14 +7,8 @@ def call(config[:]) {
 
     def pom = readMavenPom file: 'pom.xml'
 
-    withCredentials([string(credentialsId: "${githubToken}", variable: 'TOKEN')]) {
-        return sh(script: """
-            mvn -B -DskipTests=${skipTest} -s ${settingsXml} deploy
-            git config user.email "jhng323@gmail.com"
-            git config user.name "prodigy323"
-            git tag -a ${pom.version} -m 'create tag ${pom.version}'
-            git push --tags https://\${TOKEN}@github.com/prodigy323/basic-springboot-project.git
-        """, returnStdout: true)
+    withCredentials([string(credentialsId: "${credentialsId}", variable: 'TOKEN')]) {
+        return sh(script: "mvn -B -DskipTests=${skipTest} -s ${settingsXml} deploy", returnStdout: true)
     }
 
 }
