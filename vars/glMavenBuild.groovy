6fe7226eb7b75d02e031842baf2041d4f2ec0853
Joe Hong <jhng323@gmail.com>
2019-03-30 09:55:05 -0400

    fix syntax for def call(config = [:]) to accept a map

--- vars/glMavenBuild.groovy
+++ vars/glMavenBuild.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(config) {
+def call(config = [:]) {
     def skipTest = config.skipTest ?: false
 
     return sh(script: "mvn -B -DskipTests=${skipTest} clean verify", returnStdout: true)
